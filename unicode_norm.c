2fbc9b0ef7753cb1788800b92c4b81ec39d585f8
Seungwoo Jo <sjosjo1204@gmail.com>
2025-12-05 17:18:32 +0900

    remove color output

--- unicode_norm.c
+++ unicode_norm.c
@@ -160,19 +160,19 @@ void print_log(char *str, enum LogLevel level) {
 
     switch (level) {
         case ERROR:
-            fprintf(stderr, "\x1B[31m%s\x1B[0m\n", str);
+            fprintf(stderr, "%s\n", str);
             break;
         case WARNING:
-            fprintf(stderr, "\x1B[33m%s\x1B[0m\n", str);
+            fprintf(stderr, "%s\n", str);
             break;
         case INFO:
             printf("%s\n", str);
             break;
         case VERBOSE:
-            printf("\x1B[35m%s\x1B[0m\n", str);
+            printf("%s\n", str);
             break;
         case DEBUG:
-            printf("\x1B[32m%s\x1B[0m\n", str);
+            printf("%s\n", str);
             break;
     }
 }
