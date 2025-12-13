6006266acfa213e38eead04b9e9690743648f55a
Seungwoo Jo <sjosjo1204@gmail.com>
2025-12-13 22:53:20 +0900

    feat: add windows support

--- unicode_norm.c
+++ unicode_norm.c
@@ -17,8 +17,14 @@
 #include "version.h"
 
 #define STR_BUFFER 2048
-#define LOG_LEVEL LOG_INFO
-#define PATH_SEP_STR "/"
+#define LOG_LEVEL LOG_DEBUG
+#ifdef _WIN32
+  #define PATH_SEP_CHAR '\\'
+  #define PATH_SEP_STR "\\"
+#else
+  #define PATH_SEP_CHAR '/'
+  #define PATH_SEP_STR "/"
+#endif
 
 enum NormalizationForm {
     NFC,
@@ -62,7 +68,86 @@ bool rename_file(const char *file_path);
 void parse_form();
 void convert_to_form(char *path, enum NormalizationForm form, char *buffer);
 
+static int total_cnt = 0;
+static int updated_cnt = 0;
+
+#ifdef _WIN32
+#include <wchar.h>
+#include <stdbool.h>
+
+wchar_t *utf8_to_utf16(const char *s) {
+    int len = MultiByteToWideChar(CP_UTF8, 0, s, -1, NULL, 0);
+    wchar_t *w = malloc(sizeof(wchar_t) * len);
+    MultiByteToWideChar(CP_UTF8, 0, s, -1, w, len);
+    return w;
+}
+
+char *utf16_to_utf8(const wchar_t *w) {
+    if (!w) return NULL;
+
+    int size = WideCharToMultiByte(CP_UTF8, 0, w, -1, NULL, 0, NULL, NULL);
+    if (size <= 0) return NULL;
+
+    char *utf8 = (char *) malloc(size);
+    if (!utf8) return NULL;
+
+    WideCharToMultiByte(CP_UTF8, 0, w, -1, utf8, size, NULL, NULL);
+    return utf8;
+}
+
+void traverse_directory_windows(const wchar_t *dir) {
+    wchar_t search_path[MAX_PATH];
+    wchar_t logbuf[STR_BUFFER];
+    WIN32_FIND_DATAW ffd;
+    HANDLE hFind = INVALID_HANDLE_VALUE;
+    swprintf(search_path, MAX_PATH, L"%ls\\*", dir);
+    hFind = FindFirstFileW(search_path, &ffd);
+    if (hFind == INVALID_HANDLE_VALUE) {
+        swprintf(logbuf, STR_BUFFER, L"FindFirstFile failed: %ls\n", dir);
+        char* logbuf_utf8 = utf16_to_utf8(logbuf);
+        print_log(logbuf_utf8, LOG_ERROR);
+        free(logbuf_utf8);
+        return;
+    }
+
+    do {
+        const wchar_t *name = ffd.cFileName;
+        if (wcscmp(name, L".") == 0 || wcscmp(name, L"..") == 0)
+            continue;
+
+        wchar_t full_path[MAX_PATH];
+        swprintf(full_path, MAX_PATH, L"%ls\\%ls", dir, name);
+
+        bool is_dir = (ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) != 0;
+        swprintf(logbuf, STR_BUFFER, L"File entry: %ls (%ls)", full_path, is_dir ? L"dir" : L"file");
+        char* logbuf_utf8 = utf16_to_utf8(logbuf);
+        print_log(logbuf_utf8, LOG_DEBUG);
+        free(logbuf_utf8);
+
+        char *full_path_utf8 = utf16_to_utf8(full_path);
+        bool updated = rename_file(full_path_utf8);
+        updated_cnt += updated ? 1 : 0;
+        total_cnt++;
+        free(full_path_utf8);
+
+        if (is_dir) {
+            traverse_directory_windows(full_path);
+        }
+    } while (FindNextFileW(hFind, &ffd) != 0);
+
+    FindClose(hFind);
+}
+
+#endif
+
+static void split_path(const char *path, char* dir_out, size_t dir_size, char* name_out, size_t name_size);
+
 int main(const int argc, char *argv[]) {
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+#endif
+
     char string[STR_BUFFER];
     if (argc <= 1) {
         print_help();
@@ -99,15 +184,19 @@ int main(const int argc, char *argv[]) {
         sprintf(string, "File path: %s", file_path);
         print_log(string, LOG_DEBUG);
 
+#ifndef _WIN32
         FILE *f = fopen(file_path, "r");
         if (f == NULL) {
             print_log("Error: could not open file.", LOG_ERROR);
             exit(EXIT_FAILURE);
         }
+        fclose(f);
+#endif
 
         if (!is_recursive) {
             rename_file(file_path);
         } else {
+#ifndef _WIN32
             FTS *file_hierarchy = NULL;
             FTSENT *node = NULL;
             int total_cnt = 0;
@@ -131,6 +220,16 @@ int main(const int argc, char *argv[]) {
             }
 
             fts_close(file_hierarchy);
+#else
+            wchar_t *a = utf8_to_utf16(*(argv + optind));
+            traverse_directory_windows(a);
+            free(a);
+#endif
+
+            if (i < argc - 1) {
+                continue;
+            }
+
             if (is_dry_run) {
                 sprintf(string, "Total files: %d, Files that would be updated: %d", total_cnt, updated_cnt);
                 print_log(string, LOG_INFO);
@@ -138,7 +237,6 @@ int main(const int argc, char *argv[]) {
                 sprintf(string, "Total files: %d, Updated files: %d", total_cnt, updated_cnt);
                 print_log(string, LOG_INFO);
             }
-            break;
         }
     }
 
@@ -185,10 +283,11 @@ void print_log(char *str, enum LogLevel level) {
 }
 
 void convert_to_form(char *path, enum NormalizationForm form, char *buffer) {
-    char* new_basename;
+    char dir_part[PATH_MAX];
+    char name_part[PATH_MAX];
+    split_path(path, dir_part, sizeof(dir_part), name_part, sizeof(name_part));
 
-    char* dir_part = dirname(path);
-    char* name_part = basename(path);
+    char* new_basename;
     switch (form) {
         case NFC:
             new_basename = (char *) utf8proc_NFC((uint8_t *) name_part);
@@ -219,10 +318,18 @@ bool rename_file(const char *file_path) {
     char path_buffer[STR_BUFFER];
 
     char abs_path[PATH_MAX];
+
+#ifndef _WIN32
     if (realpath(file_path, abs_path) == NULL) {
         perror("realpath");
         return false;
     }
+#else
+    if (_fullpath(abs_path, file_path, PATH_MAX) == NULL) {
+        perror("_fullpath");
+        return false;
+    }
+#endif
 
     char form_string[50];
     form_string[0] = '\0';
@@ -283,4 +390,35 @@ void parse_form() {
                   "Form(--form, -f) should be one of 'NFC', 'NFD', 'NFKC', or 'NFKD'.", LOG_ERROR);
         exit(EXIT_FAILURE);
     }
+}
+
+static void split_path(const char *path, char* dir_out, size_t dir_size, char* name_out, size_t name_size) {
+    const char *last_sep = strrchr(path, PATH_SEP_CHAR);
+#ifdef _WIN32
+    const char *last_slash = strrchr(path, '/');
+    if (!last_sep || (last_slash && last_slash > last_sep)) {
+        last_sep = last_slash;
+    }
+#endif
+
+    if (!last_sep) {
+        if (dir_out && dir_size > 0) {
+            dir_out[0] = '.';
+            dir_out[1] = '\0';
+        }
+        strncpy(name_out, path, name_size - 1);
+        name_out[name_size - 1] = '\0';
+        return;
+    }
+
+    size_t dir_len = (size_t)(last_sep - path);
+    if (dir_out && dir_size > 0) {
+        if (dir_len >= dir_size) dir_len = dir_size - 1;
+        memcpy(dir_out, path, dir_len);
+        dir_out[dir_len] = '\0';
+    }
+
+    const char *name_start = last_sep + 1;
+    strncpy(name_out, name_start, name_size - 1);
+    name_out[name_size - 1] = '\0';
 }
