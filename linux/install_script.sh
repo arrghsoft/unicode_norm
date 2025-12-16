c7ae0620650c82f4731705ae9c4411d8a3a7720b
Seungwoo Jo <sjosjo1204@gmail.com>
2025-12-16 11:28:22 +0900

    feat: add linux right click action

---
+++ linux/install_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# In order to add right click action item, run the following command.
+# To remove the right click action item, remove copied item manually.
+chmod +x nautilus/scripts/Convert-to-NFC
+cp nautilus/scripts/Convert-to-NFC ~/.local/share/nautilus/scripts/Convert-to-NFC
+chmod +x nautilus/scripts/Convert-to-NFD
+cp nautilus/scripts/Convert-to-NFD ~/.local/share/nautilus/scripts/Convert-to-NFD
